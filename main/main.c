dd0ec5c20cc07eed867aa7af183031e06a44cf4b
currygood <2294008910@qq.com>
2026-07-12 22:57:31 +0800

    Release-2.1

--- main/main.c
+++ main/main.c
@@ -153,13 +153,7 @@ void app_main(void)
 		return;
 	}
 	// 整个程序只在这里写一次！
-    ESP_ERROR_CHECK(gpio_install_isr_service(0));  
-    // ESP_LOGI("APP_MAIN", "GPIO ISR 服务安装成功");  //成功就不管，没必要输出了
-
-	vTaskDelay(pdMS_TO_TICKS(500));
-
-	// 整个程序只在这里写一次！
-    ESP_ERROR_CHECK(gpio_install_isr_service(0));  
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
     // ESP_LOGI("APP_MAIN", "GPIO ISR 服务安装成功");  //成功就不管，没必要输出了
 
 	vTaskDelay(pdMS_TO_TICKS(500));
